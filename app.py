1493620b36382a9eeb88f8266744012f27387d0b
shanconsult1987 <78853500+shanconsult1987@users.noreply.github.com>
2026-03-10 12:20:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "CodeQL Demo App"
+
+@app.route("/user")
+def get_user():
+
+    user = request.args.get("id")
+
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM users WHERE id = " + user
+
+    cursor.execute(query)
+
+    result = cursor.fetchall()
+
+    return str(result)
+
+app.run(host="0.0.0.0", port=5000)
